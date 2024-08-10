abf9794fe724145a0b769ed56019407bffc21586
MauricioJC3 <a.mauriciojc03@gmail.com>
2024-08-09 23:33:48 -0500

    Se añadiran los tipos de datos y las variables con su respectiva descripcion

---
+++ PrimeraPractica/primeraParte.js
@@ -0,0 +1,9 @@
+
+                                                ///////////////////////////////
+                                                /***      Tipos de Datos    ***/
+                                                //////////////////////////////
+
+                                                // 1. Numéricos:
+                                                // - Enteros: int (8 bits)
+                                                // - Decimales: float (32 bits)
+                                                // - Largos enteros: long (64 bits)
