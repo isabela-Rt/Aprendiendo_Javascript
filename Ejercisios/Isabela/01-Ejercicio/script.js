22398f36d8444f42785168345790cd3036b40236
isabela-Rt <isatejada048@gmail.com>
2024-08-23 23:02:01 -0500

    Renombramiento de archivos y inicio de ejercicio 1

---
+++ Ejercisios/Isabela/01-Ejercicio/script.js
@@ -0,0 +1,43 @@
+// Este es un comentario de una sola línea
+
+/**
+ * Este es un comentario de varias líneas
+ *           PERSONAS MÁGICAS
+ * Las personas mágicas existen, Aparecen de la nada.
+ * Son aquellos que saben ganarse la confianza rápido y
+ * inesperada. De pronto un día llegan a tu vida, 
+ * empiezan a hablar de todo, de alegrías, daños, experiencias, 
+ * penas y heridas.
+ * 
+ * Cuando te das cuenta, no recuerdas como era tu vida antes de
+ * que la conocieras. Así son las personas mágicas.
+ * 
+ * Y allí están, llegan a tu universo para darte un aire más liviano,
+ * para brindarte su amistad, su mano y elevar su energía juntos.
+ * Llegan para abrazarte y muchas veces para quedarse.
+ */
+
+//Number
+let edad = 18 // Entero
+let altura = 1.57 // Decimal
+
+//String
+let texto = "Hola soy Isabela Rubio Tejada";
+
+//Boolean
+let Megustalamusica = true;
+let Nomegusta = false;
+
+//Undefined 
+let undef= undefined;
+
+//Null
+let nula = null;
+
+console.log('Edad:', edad);
+console.log('Altura:', altura);
+console.log('Texto:', texto);
+console.log('Me gusta la música:', Megustalamusica);
+console.log('No me gusta:', Nomegusta);
+console.log('Undefined:', undef);
+console.log('Null:', nula);
