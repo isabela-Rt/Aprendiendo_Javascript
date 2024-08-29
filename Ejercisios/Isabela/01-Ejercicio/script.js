4b97790c95f7a3ff1423f5a7246f844b6b1872f0
isabela-Rt <isatejada048@gmail.com>
2024-08-28 22:58:03 -0500

    Terminación del ejercicio 1

--- Ejercisios/Isabela/01-Ejercicio/script.js
+++ Ejercisios/Isabela/01-Ejercicio/script.js
@@ -17,7 +17,8 @@
  * Llegan para abrazarte y muchas veces para quedarse.
  */
 
-//Number
+/**
+ * //Number
 let edad = 18 // Entero
 let altura = 1.57 // Decimal
 
@@ -41,3 +42,106 @@ console.log('Me gusta la música:', Megustalamusica);
 console.log('No me gusta:', Nomegusta);
 console.log('Undefined:', undef);
 console.log('Null:', nula);
+ */
+
+/**
+//Number
+let edad = 25; // Entero
+let altura = 1.75; // Decimal
+
+//String
+let texto = "Hola, mi nombre es Ana luz Gómez";
+
+//Boolean
+let Megustalamusica = false;
+let Nomegusta = true;
+
+//Undefined 
+let undef = undefined;
+
+//Null
+let nula = null;
+
+console.log('Edad:', edad);
+console.log('Altura:', altura);
+console.log('Texto:', texto);
+console.log('Me gusta la música:', Megustalamusica);
+console.log('No me gusta:', Nomegusta);
+console.log('Undefined:', undef);
+console.log('Null:', nula);
+*/
+
+/**
+//Number
+let edad = "veinticinco"; // String (en lugar de entero)
+let altura = true; // Boolean (en lugar de decimal)
+
+//String
+let texto = 12345; // Number (en lugar de string)
+
+//Boolean
+let Megustalamusica = "Sí"; // String (en lugar de booleano)
+let Nomegusta = 0; // Number (en lugar de booleano)
+
+//Undefined 
+let undef = "no definido"; // String (en lugar de undefined)
+
+//Null
+let nula = 100; // Number (en lugar de null)
+
+console.log('Edad:', edad);
+console.log('Altura:', altura);
+console.log('Texto:', texto);
+console.log('Me gusta la música:', Megustalamusica);
+console.log('No me gusta:', Nomegusta);
+console.log('Undefined:', undef);
+console.log('Null:', nula);
+*/
+
+/**
+ * // Number
+const edad = 18; // Entero
+const altura = 1.57; // Decimal
+
+// String
+const texto = "Hola soy Isabela Rubio Tejada";
+
+// Boolean
+const me_gusta_la_musica = true;
+const no_me_gusta = false;
+
+// Undefined 
+const undefined_value = undefined;
+
+// Null
+const nula = null;
+
+console.log('Edad:', edad);
+console.log('Altura:', altura);
+console.log('Texto:', texto);
+console.log('Me gusta la música:', me_gusta_la_musica);
+console.log('No me gusta:', no_me_gusta);
+console.log('Undefined:', undefined_value);
+console.log('Null:', nula);
+
+ */
+
+/**
+// Modificación de valores en las constantes
+edad = 25;
+altura = 1.75;
+texto = "Hola, soy Pepita";
+meGustaLaMusica = false;
+noMeGusta = true;
+undef = "Ahora tiene un valor definido";
+nula = "Ya no es null";
+
+console.log('Edad:', edad);
+console.log('Altura:', altura);
+console.log('Texto:', texto);
+console.log('Me gusta la música:', meGustaLaMusica);
+console.log('No me gusta:', noMeGusta);
+console.log('Undefined:', undef);
+console.log('Null:', nula);
+
+*/
